3a0942b2f754b197861b06690c75a1415fab46bd
Lance0313 <lncdlrsr@gmail.com>
2024-08-02 14:12:47 +0800

    Virtual Calling integrated vueJS

---
+++ webProject/app.js
@@ -0,0 +1,19 @@
+Vue.createApp({
+    data() {
+        return {
+            profilePic: "/webProject/resources/images/employee profile/lloydprofile.jpg",
+            companyPic: "/webProject/resources/images/company logo/hyundaibinan.jpg",
+            employeeName: "Lloyd Arwin Jhon Poblete",
+            employeeRole: "Technical Trainer",
+            employeeCompany: "Hyundai Biñan Laguna",
+            employeeEmail: "trainer.arezamotor@gmail.com",
+            employeeMobile: "09569848751",
+
+
+        };
+    },
+    methods:{
+
+    }
+
+}).mount('#body');
